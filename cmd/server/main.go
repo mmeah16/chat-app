ec154ed16567ee627fc84605e0a3517e989324dc
Muntasir Meah <muntasirmeah@Muntasirs-Air.home>
2025-12-21 18:58:08 -0500

    Wiring DB and new routes to main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"chat/internal/config"
+	"chat/internal/db"
+	"chat/internal/handlers"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  // Create a Gin router with default middleware (logger and recovery)
-  r := gin.Default()
 
-  // Define a simple GET endpoint
-  r.GET("/health", healthCheck)
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
 
-  // Start server on port 8080 (default)
-  // Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
-  r.Run()
+	log.Println(cfg.DBName)
+
+  	database, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer database.Close()
+	
+	// Create a Gin router with default middleware (logger and recovery)
+	r := gin.Default()
+
+	// Method 2: Use middleware to inject db
+	r.Use(func(c *gin.Context) {
+		c.Set("db", database)
+		c.Next()
+	})
+
+	// Define a simple GET endpoint
+	r.GET("/health", healthCheck)
+
+	// Start server on port 8080 (default)
+	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
+	handlers.RegisterRoutes(r) 
+	r.Run()
 }
 
 func healthCheck(c *gin.Context) {
--- db/migrations/000001_create_users_table.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS messages;
-DROP TABLE IF EXISTS conversation_members;
-DROP TABLE IF EXISTS conversations;
-DROP TABLE IF EXISTS users;
